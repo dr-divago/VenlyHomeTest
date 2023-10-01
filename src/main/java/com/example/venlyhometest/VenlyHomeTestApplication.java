73b6cc38f9a7676e25a4e5741fbede2cdbdfa82c
dr-divago <matteo.pelliccia@gmail.com>
2023-10-01 13:56:23 +0200

    Initial commit

---
+++ src/main/java/com/example/venlyhometest/VenlyHomeTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.venlyhometest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VenlyHomeTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VenlyHomeTestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/venlyhometest/VenlyHomeTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.venlyhometest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VenlyHomeTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
