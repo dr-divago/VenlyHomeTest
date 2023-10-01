dc012dac3cf0fb0fa7769eef20a1cd2468b825db
dr-divago <matteo.pelliccia@gmail.com>
2023-10-01 16:36:37 +0200

    task4

--- src/main/java/com/example/venlyhometest/controller/WordController.java
+++ src/main/java/com/example/venlyhometest/controller/WordController.java
@@ -6,6 +6,7 @@ import com.example.venlyhometest.dto.RelationshipResponseJson;
 import com.example.venlyhometest.dto.RelationshipType;
 import com.example.venlyhometest.entity.Relationship;
 import com.example.venlyhometest.repo.RelationRepository;
+import com.example.venlyhometest.service.WordService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,6 +16,7 @@ import javax.validation.Valid;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
 
 @RestController
@@ -22,23 +24,24 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class WordController {
 
-    private final RelationRepository relationRepository;
-
+    private final WordService wordService;
     @PostMapping("/add")
     public ResponseEntity<Void> addRelation(@Valid @RequestBody  RelationshipRequest request) {
-        Relationship relationship = new Relationship(request.getW1(), request.getW2(), request.getType());
-        relationRepository.save(relationship);
+        wordService.addRelation(request);
         return ResponseEntity.status(HttpStatus.CREATED).build();
     }
 
     @GetMapping("/get-words-relation")
-    public ResponseEntity<List<RelationshipResponseJson>> getRelation(@RequestParam(name = "filterByRelation", required = false) String r) {
+    public ResponseEntity<List<RelationshipResponseJson>> getRelation(
+            @RequestParam(name = "filterByRelation", required = false) String r,
+            @RequestParam(name = "inverse", required = false) Boolean inverse) {
+        Optional<Boolean> maybeInverse = Optional.ofNullable(inverse);
         RelationshipType searchRelation = RelationshipType.valueOf(Optional.ofNullable(r).orElse("none").toUpperCase());
 
-        List<RelationshipResponseJson> relationship = relationRepository.findAll().stream()
-                .filter( x -> searchRelation.equals(RelationshipType.NONE) || x.getRelationshipType().equals(searchRelation))
-                .map( x -> new RelationshipResponseJson(x.getW1(), x.getW2(), x.getRelationshipType()))
-                .collect(Collectors.toList());
+        List<RelationshipResponseJson> relationship = wordService.getRelationshipResponseJsons(maybeInverse, searchRelation);
+
         return ResponseEntity.ok(relationship);
     }
+
+
 }
